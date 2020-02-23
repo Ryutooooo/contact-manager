524b852ca9de4fea9e4aa02f4ea7d7488ddfb440
Ryutooooo <rf.ryutooooo.0907@gmail.com>
2020-02-23 23:42:05 +0900

    feat: create top

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,19 @@
+import {Router, RouterConfiguration} from 'aurelia-router';
+import {PLATFORM} from 'aurelia-pal';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+
 export class App {
-  public message: string = 'Hello World!';
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    config.title = 'Contacts';
+    config.map([
+      { route: '', moduleId: PLATFORM.moduleName('no-selection'), title: 'Select'},
+      { route: 'contacts/:id', moduleId: PLATFORM.moduleName('contact-detail'), name:'contacts' }
+    ]);
+
+    this.router = router;
+  }
 }
