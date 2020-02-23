a5b8d4706efd598e631a59ee8e0f804a80da3e36
Ryutooooo <rf.ryutooooo.0907@gmail.com>
2020-02-24 03:06:10 +0900

    feat: conclustion lessions

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,23 @@
 import {Router, RouterConfiguration} from 'aurelia-router';
-import {PLATFORM} from 'aurelia-pal';
+import {inject, PLATFORM} from 'aurelia-framework';
+import {WebAPI} from './web-api';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
-
-export class App {
-  router: Router;
-
-  configureRouter(config: RouterConfiguration, router: Router) {
-    config.title = 'Contacts';
-    config.map([
-      { route: '', moduleId: PLATFORM.moduleName('no-selection'), title: 'Select'},
-      { route: 'contacts/:id', moduleId: PLATFORM.moduleName('contact-detail'), name:'contacts' }
-    ]);
-
-    this.router = router;
-  }
-}
+ 
+ @inject(WebAPI)
+ export class App {
+   router: Router;
+ 
+   constructor(public api: WebAPI) {}
+ 
+   configureRouter(config: RouterConfiguration, router: Router) {
+     config.title = 'Contacts';
+     config.map([
+       { route: '',              moduleId: PLATFORM.moduleName('no-selection'),   title: 'Select'},
+       { route: 'contacts/:id',  moduleId: PLATFORM.moduleName('contact-detail'), name:'contacts' }
+     ]);
+ 
+     this.router = router;
+   }
+ }
